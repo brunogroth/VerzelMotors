93560bf72e17fa7bc71aee75fec009f4d0d518ab
Bruno Groth <brunom.groth@gmail.com>
2022-11-20 18:11:07 -0300

    fix: imagePath for vehicle creation

--- APIVerzelMotors/src/app/useCases/vehicles/createVehicle.ts
+++ APIVerzelMotors/src/app/useCases/vehicles/createVehicle.ts
@@ -3,7 +3,8 @@ import { Vehicle } from "../../models/Vehicle";
 
 export async function createVehicle(req: Request, res: Response) {
   try {
-    const { name, makeId, model, price, imagePath } = req.body;
+    const { name, makeId, model, price } = req.body;
+    const imagePath = req.file?.filename;
 
     const vehicle = await Vehicle.create({ name, makeId, model, price, imagePath });
 
